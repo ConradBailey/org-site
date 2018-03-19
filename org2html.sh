1c0c5337d74505351a6dc2bc00aa54e10531f6f5
Conrad Bailey <conradbailey92@gmail.com>
2018-03-19 19:53:14 -0400

    fixed youtube links

--- org2html.sh
+++ org2html.sh
@@ -1,6 +1,31 @@
 #!/bin/sh
 ":"; exec emacs --quick --script "$0" "$@" # -*-emacs-lisp-*-
 
+(require 'org);
+
+;; Add an embedded youtube link to org-mode (http://endlessparentheses.com/embedding-youtube-videos-with-org-mode-links.html)
+(defvar yt-iframe-format
+  ;; You may want to change your width and height.
+  (concat "<iframe width=\"440\""
+          " height=\"335\""
+          " src=\"https://www.youtube.com/embed/%s\""
+          " frameborder=\"0\""
+          " allowfullscreen>%s</iframe>"))
+
+(org-add-link-type
+ "yt"
+ (lambda (handle)
+   (browse-url
+    (concat "https://www.youtube.com/embed/"
+            handle)))
+ (lambda (path desc backend)
+   (cl-case backend
+     (html (format yt-iframe-format
+                   path (or desc "")))
+     (latex (format "\href{%s}{%s}"
+                    path (or desc "video"))))))
+
+
 (setq input-filename (pop argv))
 (when argv
 	(princ "org2html: Too many parameters provided")
